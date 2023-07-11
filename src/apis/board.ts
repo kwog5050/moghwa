005267bd4d6dc5b753eeeefb1c86c30b87d0afc3
jung woo Kwon <kwog5050@naver.com>
2023-07-11 15:24:52 +0900

    modified:   src/apis/board.ts

--- src/apis/board.ts
+++ src/apis/board.ts
@@ -6,6 +6,7 @@ import { BoardId, Board, GetBoard, UpdataBoard } from 'interfaces/board';
 const createBoard = async (data: Board): Promise<void> => {
     try {
         const res = await axios.post('/board', data);
+        axiosInspection(res.data, '알 수 없는 에러로 게시글 작성에 실패했습니다.');
     } catch (error) {
         handleApiError(error);
     }
@@ -23,6 +24,7 @@ const getBoard = async (success: (data: GetBoard[]) => void): Promise<void> => {
 const updataBoard = async (data: UpdataBoard): Promise<void> => {
     try {
         const res = await axios.patch('/board', data);
+        axiosInspection(res.data, '알 수 없는 에러로 게시글 수정에 실패했습니다.');
     } catch (error) {
         handleApiError(error);
     }
@@ -31,6 +33,7 @@ const updataBoard = async (data: UpdataBoard): Promise<void> => {
 const deleteBoard = async (data: BoardId): Promise<void> => {
     try {
         const res = await axios.delete(`/board/${data.id}`);
+        axiosInspection(res.data, '알 수 없는 에러로 게시글 삭제 실패했습니다.');
     } catch (error) {
         handleApiError(error);
     }
